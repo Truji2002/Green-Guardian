3f7f5711e58e5394409562cda20c3884269a9023
Truji2002 <111396764+Truji2002@users.noreply.github.com>
2023-04-27 22:21:07 -0500

    Código Semilla en Suelo Apto para crecer arboles
    Se agrega el código para que una semilla crezca en un suelto apto y se convierta en árbol.  En el caso de que no sea apto se destruirá la semilla en cierto tiempo.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    
     public Rigidbody projectile;
     public Transform mySpawn;
     public float fireRate = 0.5f; // The time (in seconds) between shots
+    public float bulletLifetime = 5f; // The time (in seconds) that bullets will exist before being destroyed
+    public float maxDistance = 50f; // The maximum distance (in units) that bullets can travel
+    public GameObject treePrefab; // The prefab for the tree object
+    public Material seedMaterial; // The material for the seed object
     private float timer = 0f; // The timer that keeps track of the time elapsed
-
 
     void Update()
     {
@@ -24,8 +26,21 @@ public class Shooter : MonoBehaviour
 
     void Shoot()
     {
+        float speed = maxDistance / bulletLifetime; // Calculate the speed required to travel the maximum distance in the given lifetime
         Rigidbody bullet = Instantiate(projectile, mySpawn.position, mySpawn.rotation);
-        bullet.velocity = mySpawn.forward * 50;
-        
-    }
+        bullet.velocity = mySpawn.forward * speed; // Set the velocity of the bullet to the required speed
+        bullet.GetComponent<Renderer>().material = seedMaterial; // Set the material of the bullet to the seed material
+        Destroy(bullet.gameObject, bulletLifetime); // Destroy the bullet after bulletLifetime seconds
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("SueloApto"))
+        {
+            GameObject tree = Instantiate(treePrefab, collision.contacts[0].point, Quaternion.identity);
+            tree.GetComponent<Renderer>().material.color = Color.green; // Change the color of the tree
+            Destroy(gameObject); // Destroy the bullet
+            //Destroy(tree, bulletLifetime); // Destroy the tree after bulletLifetime seconds
+        }
+    }
 }
