7bd9572679a246fe9a0c1ba15806f5253f738990
Truji2002 <111396764+Truji2002@users.noreply.github.com>
2023-05-18 23:44:57 -0500

    Faltan 2 funcionalidades

--- Assets/Scripts/DatosJugador.cs
+++ Assets/Scripts/DatosJugador.cs
@@ -2,24 +2,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
+//using TMPro;
+
 
 
 public class DatosJugador : MonoBehaviour
 {
-    // Start is called before the first frame update
     public int vidaPlayer;
     public Slider vidaVisual;
-    
+    public Vector3 posicion;
+    public int muertes;
 
-    // Update is called once per frame
+  
+
+    void Start()
+    {
+        // Cargamos la posicion inicial desde un archivo JSON si existe
+        string path = Application.persistentDataPath + "/posicion.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            posicion = JsonUtility.FromJson<Vector3>(json);
+        }
+        else
+        {
+            // Guardamos la posicion inicial en un archivo JSON
+            posicion = transform.position;
+            string json = JsonUtility.ToJson(posicion);
+            File.WriteAllText(path, json);
+        }
+
+        // Reseteamos la vida y las muertes
+        vidaPlayer = 100;
+        muertes = 0;
+    }
+
     void Update()
     {
+        
 
-        vidaVisual.GetComponent<Slider>().value=vidaPlayer;
-        if(vidaPlayer <=0)
-
+        vidaVisual.GetComponent<Slider>().value = vidaPlayer;
+        if (vidaPlayer <= 0)
         {
-        SceneManager.LoadScene("Game Over");
+            muertes++;
+            if (muertes < 2)
+            {
+                // Revivimos al jugador
+                vidaPlayer = 100;
+                transform.position = posicion;
+            }
+            else
+            {
+                Debug.Log(Application.persistentDataPath);
+
+               SceneManager.LoadScene("Game Over");
+            }
         }
+        // Obtener el componente TextMeshPro del objeto Canvas
+        //TextMeshProUGUI textoVidas = GetComponentInChildren<TextMeshProUGUI>();
+
+        // Actualizar el texto con el número de vidas restantes
+        //textoVidas.text = "Vidas restantes: " + (2 - muertes).ToString();
     }
+       
 }
